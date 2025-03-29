c55427eb11da90f7bdd048eb3e5ff844f61c4b58
Bridgette Rue <bridgetterue@egg.design>
2025-03-29 18:01:39 -0500

    testing

--- src/components/search-settings/ResumeSession.tsx
+++ src/components/search-settings/ResumeSession.tsx
@@ -7,8 +7,8 @@ export interface ResumeProps {
     onResume: any
 }
 const ResumeSession = ({ lastState, onResume }: ResumeProps) => {
-    // if (lastState.tag === '' || lastState.tag == null) { return null }
-    // const date = new Date((parseInt(lastState.timestamp)) * 1000) ?? new Date()
+    if (lastState.tag === '' || lastState.tag == null) { return null }
+    const date = new Date((parseInt(lastState.timestamp)) * 1000) 
     // const day = date.toString() === 'Invalid Date' ? '' : 'before ' + date.toLocaleDateString('en-US', { day: 'numeric', weekday: 'short', year: 'numeric', month: 'short' })
 
     return (
