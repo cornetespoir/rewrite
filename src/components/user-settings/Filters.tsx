885e967e1a91ece4da77b7d0f2a513a398d63c11
Bridgette Rue <bridgetterue@egg.design>
2025-03-29 17:10:27 -0500

    added a bunch of states and component changes

--- src/components/user-settings/Filters.tsx
+++ src/components/user-settings/Filters.tsx
@@ -16,7 +16,7 @@ const Filters = () => {
                         {filter}
                         <button style={{ cursor: "pointer" }}
                             onClick={() => deletefilter(id)}>
-                            <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" className="feather feather-trash-2">
+                            <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round" className="feather feather-trash-2">
                                 <polyline points="3 6 5 6 21 6" />
                                 <path d="M19 6v14a2 2 0 0 1-2 2H7a2 2 0 0 1-2-2V6m3 0V4a2 2 0 0 1 2-2h4a2 2 0 0 1 2 2v2" />
                                 <line x1="10" y1="11" x2="10" y2="17" />
--- src/hooks/useEventListener.ts
+++ src/hooks/useEventListener.ts
@@ -3,14 +3,22 @@ import { useEffect, type RefObject } from 'react'
 const useFventListener = <T extends HTMLElement | Document = HTMLDivElement>(
     eventName: keyof WindowEventMap,
     handler: (e: Event) => void,
-    elementRef: RefObject<T>
+    elementRef?: RefObject<T>
 ) => {
     useEffect(() => {
-        const { current: element } = elementRef
-        if (!element || !element.addEventListener) return
-        element.addEventListener(eventName, handler)
-        return () => {
-            element.removeEventListener(eventName, handler)
+        if (elementRef != null) {
+            const { current: element } = elementRef
+            if (!element || !element.addEventListener) return
+            element.addEventListener(eventName, handler)
+            return () => {
+                element.removeEventListener(eventName, handler)
+            }
+        }
+        else {
+            window.addEventListener(eventName, handler)
+            return () => {
+                window.removeEventListener(eventName, handler)
+            }
         }
     }, [eventName, elementRef, handler])
 }
