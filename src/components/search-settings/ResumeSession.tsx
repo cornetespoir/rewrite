09cdb395b4fd3749ee03b7c16d39cd8bbf1f4e50
Bridgette Rue <bridgetterue@egg.design>
2025-03-29 18:11:15 -0500

    finally. i completely forgot to check for the entire previous state vs just the tag name. wow.

--- src/components/search-settings/ResumeSession.tsx
+++ src/components/search-settings/ResumeSession.tsx
@@ -9,19 +9,15 @@ export interface ResumeProps {
 const ResumeSession = ({ lastState, onResume }: ResumeProps) => {
     if (lastState == null) {return null}
     if (lastState.tag === '' || lastState.tag == null) { return null }
-    console.log('last state', lastState)
-
-    // const date = new Date((parseInt(lastState.timestamp ?? 0)) * 1000)
-    // console.log(date)
-    // const day = date.toString() === 'Invalid Date' ? '' : 'before ' + date.toLocaleDateString('en-US', { day: 'numeric', weekday: 'short', year: 'numeric', month: 'short' })
+    const date = new Date((parseInt(lastState.timestamp)) * 1000)
+    const day = date.toString() === 'Invalid Date' ? '' : 'before ' + date.toLocaleDateString('en-US', { day: 'numeric', weekday: 'short', year: 'numeric', month: 'short' })
 
     return (
         <div className='options'>
             <div className='last-session'>
                 <h3>Continue where you left off?</h3>
-                <p>this is just a test</p>
-                {/* <p>Your last detected search was for posts tagged <span>{lastState.tag}</span> {day}</p>
-                <button onClick={onResume}>Resume your last search</button> */}
+                <p>Your last detected search was for posts tagged <span>{lastState.tag}</span> {day}</p>
+                <button onClick={onResume}>Resume your last search</button>
             </div>
 
         </div>
