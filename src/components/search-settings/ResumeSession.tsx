190fb76cc736c362c3040a11fc03779995ce3259
Bridgette Rue <bridgetterue@egg.design>
2025-03-29 17:48:53 -0500

    i dont wanna do this. but i'm gonna try.

--- src/components/search-settings/ResumeSession.tsx
+++ src/components/search-settings/ResumeSession.tsx
@@ -1,44 +1,26 @@
 "use client"
-import { SearchContext } from "@/app/SearchContext";
-import { AppRouterInstance } from "next/dist/shared/lib/app-router-context.shared-runtime";
-import { usePathname, useSearchParams } from "next/navigation";
-import { useContext, useState } from "react";
+import { LastState } from "@/app/types";
+
 
 export interface ResumeProps {
-    router: AppRouterInstance
+    lastState: LastState
+    onResume: any
 }
-const ResumeSession = ({router}: ResumeProps) => {
-    const {lastState, setTag, setTimestamp} = useContext(SearchContext)
-    const pathname = usePathname();
-    const searchParams = useSearchParams();
-
-    const onResume = () => {
-        const params = new URLSearchParams(searchParams);
-        params.set('tag', `${lastState.tag}`);
-        params.set('before', `${lastState.timestamp}`);
-        router.push(pathname + '?' + params)
-        setTag(lastState.tag)
-        setTimestamp(lastState.timestamp)
-    }
-    const options = {
-        weekday: "long",
-        year: "numeric",
-        month: "long",
-        day: "numeric",
-      };
+const ResumeSession = ({ lastState, onResume }: ResumeProps) => {
+    if (lastState.tag === '' || lastState.tag == null) { return null }
     const date = new Date((parseInt(lastState.timestamp)) * 1000) ?? new Date()
-    const day = date.toString() === 'Invalid Date' ? '': 'before ' + date.toLocaleDateString('en-US', { day: 'numeric', weekday: 'short', year: 'numeric', month: 'short' })
+    const day = date.toString() === 'Invalid Date' ? '' : 'before ' + date.toLocaleDateString('en-US', { day: 'numeric', weekday: 'short', year: 'numeric', month: 'short' })
 
     return (
         <div className='options'>
             <div className='last-session'>
-            <h3>Continue where you left off?</h3>
-            <p>Your last detected search was for posts tagged <span>{lastState.tag}</span> {day}</p>
-            <button onClick={onResume}>Resume your last search</button>
+                <h3>Continue where you left off?</h3>
+                <p>Your last detected search was for posts tagged <span>{lastState.tag}</span> {day}</p>
+                <button onClick={onResume}>Resume your last search</button>
             </div>
-           
+
         </div>
     )
 }
 
-export {ResumeSession}
+export { ResumeSession }
