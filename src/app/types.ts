71e0b13de1feaf07b3d8ab0437c88fd46ae7e58f
Bridgette Rue <bridgetterue@egg.design>
2025-03-28 22:49:40 -0500

    setting things up

---
+++ src/app/types.ts
@@ -0,0 +1,38 @@
+export type PostResults = {
+    response: []
+}
+
+export interface UserData {
+    name?: string
+    title?: string
+    url?: string
+    
+}
+
+export interface PostData {
+    blog: {}
+    errors?: {}
+    blog_name?: string
+    body?: string
+    date?: string
+    id: number
+    note_count: number
+    post_url: string
+    reblog_key: string
+    tags?: []
+    title?: string
+    type: string
+    photos?: []
+    answer?: string
+    asking_name?: string
+    asking_url?: string
+    question?: string
+    timestamp: number
+    interactability_reblog?: string
+    source_url?: string
+    summary?: string
+    source_title?: string,
+    description?: string,
+    caption?: string,
+    player?: []
+}
