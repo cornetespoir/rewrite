67a4c615478ebfb535b3a1c714e8b38a8f8658c2
Bridgette Rue <bridgetterue@egg.design>
2025-03-28 22:50:16 -0500

    creating main pages

---
+++ src/app/pages/faq/page.tsx
@@ -0,0 +1,14 @@
+import Image from "next/image";
+import styles from "./page.module.css";
+
+export default function Home() {
+  return (
+    <main id='faq-page'>
+    <h1>FAQ</h1>
+    <article id='faq-fivetags'>
+        <h2>Why only the first 5 tags?</h2>
+        <p>Findtags uses Tumblr's API, which limits the results to the first 5 tags. There isn't a way to avoid this (that I'm currently aware of), but if that changes, I'll update it!</p>
+    </article>
+    </main>
+  )
+}
