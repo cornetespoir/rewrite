f228c68ab0e9586725d617a42fdbb5475bda0cda
Bridgette Rue <bridgetterue@egg.design>
2025-03-29 17:58:07 -0500

    ignore me at this point

--- src/components/search-settings/ResumeSession.tsx
+++ src/components/search-settings/ResumeSession.tsx
@@ -7,19 +7,23 @@ export interface ResumeProps {
     onResume: any
 }
 const ResumeSession = ({ lastState, onResume }: ResumeProps) => {
-    if (lastState.tag === '' || lastState.tag == null) { return null }
-    const date = new Date((parseInt(lastState.timestamp)) * 1000) ?? new Date()
-    const day = date.toString() === 'Invalid Date' ? '' : 'before ' + date.toLocaleDateString('en-US', { day: 'numeric', weekday: 'short', year: 'numeric', month: 'short' })
+    // if (lastState.tag === '' || lastState.tag == null) { return null }
+    // const date = new Date((parseInt(lastState.timestamp)) * 1000) ?? new Date()
+    // const day = date.toString() === 'Invalid Date' ? '' : 'before ' + date.toLocaleDateString('en-US', { day: 'numeric', weekday: 'short', year: 'numeric', month: 'short' })
+
+    // return (
+    //     <div className='options'>
+    //         <div className='last-session'>
+    //             <h3>Continue where you left off?</h3>
+    //             <p>Your last detected search was for posts tagged <span>{lastState.tag}</span> {day}</p>
+    //             <button onClick={onResume}>Resume your last search</button>
+    //         </div>
+
+    //     </div>
+    // )
 
     return (
-        <div className='options'>
-            <div className='last-session'>
-                <h3>Continue where you left off?</h3>
-                <p>Your last detected search was for posts tagged <span>{lastState.tag}</span> {day}</p>
-                <button onClick={onResume}>Resume your last search</button>
-            </div>
-
-        </div>
+        <> placeholder test</>
     )
 }
 
