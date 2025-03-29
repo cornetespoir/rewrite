02613684eae0418c8bd146070ec9a4d08e2f321a
Bridgette Rue <bridgetterue@egg.design>
2025-03-28 23:09:30 -0500

    i'm not a clown. i'm the entire circus.

--- src/app/pages/faq/page.tsx
+++ src/app/pages/faq/page.tsx
@@ -1,10 +1,11 @@
+"use client"
 export default function Home() {
   return (
     <main id='faq-page'>
     <h1>FAQ</h1>
     <article id='faq-fivetags'>
         <h2>Why only the first 5 tags?</h2>
-        <p>Findtags uses Tumblr's API, which limits the results to the first 5 tags. There isn't a way to avoid this (that I'm currently aware of), but if that changes, I'll update it!</p>
+        <p>Findtags uses Tumblr&apos;s API, which limits the results to the first 5 tags. There isn&apos;t a way to avoid this (that I&apos;m currently aware of), but if that changes, I&apos;ll update it!</p>
     </article>
     </main>
   )
